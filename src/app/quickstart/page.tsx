6a2750bc1afd2dc8301cbdf671dad9a97198e754
Kiet Ho <hoakiet98@gmail.com>
2024-09-30 09:05:42 -0400

    Add get-started page

--- src/app/quickstart/page.tsx
+++ src/app/quickstart/page.tsx
@@ -4,11 +4,17 @@ export default function DocumentationPage() {
       <div className="flex justify-start pl-[10px] font-semibold h-full items-start border w-1/5">
         <p className="w-full h-[40px] flex items-center">Quick Start</p>
       </div>
-      <div className="w-full h-full border">
-        <div className="w-full h-[80px] flex justify-center items-center">
+      <div className="w-full h-full border flex flex-col gap-[20px] p-[60px]">
+        <div className="w-full flex items-start justify-start flex-col h-[fit-content]">
           <p className="w-[fit-content] h-[fit-content] font-bold text-[36px]">
             Quick Start
           </p>
+          <p className="w-[fit-content] h-[fit-content]">
+            Start building modern documentation in under five minutes
+          </p>
+        </div>
+        <div className="w-[824px] h-[385px] bg-[#E3F3FF] flex justify-center items-center">
+          <p className="w-[fit-content] h-[fit-content]">Placeholder image</p>
         </div>
       </div>
     </div>
